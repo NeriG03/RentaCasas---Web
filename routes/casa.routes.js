f66d4cdf7cb1ad51b185f487e47adf523bee62e2
NeriG03 <sosavillanuevan@gmail.com>
2024-11-29 15:00:54 -0600

    Remove token verification from casa routes for user-related endpoints

--- routes/casa.routes.js
+++ routes/casa.routes.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import CasaController from '../controllers/casa.controller.js';
-import { verifyToken } from '../middlewares/jwt.middleware.js';
+//import { verifyToken } from '../middlewares/jwt.middleware.js';
 
 const router = express.Router();
 
 router
     .get('/casa', CasaController.get)
     .get('/casa/:id', CasaController.getById)
-    .get('/casa/user/:id', verifyToken, CasaController.getByUserEmail)
-    .post('/casa', verifyToken ,CasaController.post)
-    .put('/casa/:id', verifyToken ,CasaController.put)
-    .delete('/casa/:id', verifyToken ,CasaController.deleteCasa);
+    .get('/casa/user/:id', CasaController.getByUserEmail)
+    .post('/casa' ,CasaController.post)
+    .put('/casa/:id' ,CasaController.put)
+    .delete('/casa/:id' ,CasaController.deleteCasa);
 
 export default router;
